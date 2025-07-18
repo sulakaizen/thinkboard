5ea9c5384a13c9d03c3e006fdac63ee7e770b29f
Sultan <sulacoder@gmail.com>
2025-07-18 23:29:59 +0500

    Project setup

---
+++ backend/routes/notesRoutes.js
@@ -0,0 +1,20 @@
+import express from "express"
+
+const router = express.Router();
+router.get("/", (req,res) => {
+    res.status(200).send("You just fetched the notes");
+});
+
+router.post("/", (req,res) => {
+    res.status(200).json({message: "Note created successfully!"})
+})
+
+router.put("/:id", (req,res) => {
+    res.status(200).json({message: "Note updated successfully!"})
+})
+
+router.delete("/:id", (req,res) => {
+    res.status(200).json({message: "Note deleted successfully!"})
+})
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+
+const app = express();
+
+app.use("/api/notes", notesRoutes); // it will hit to notesRoutes
+
+//What is an Endpoint?
+// An endpoint is a combination of a URL + HTTP method that lets the client interact with a specific resource
+
+app.listen(5001, () => {
+    console.log("Server started on PORT: 5001");
+});
